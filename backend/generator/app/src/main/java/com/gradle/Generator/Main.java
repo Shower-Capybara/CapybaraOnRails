011f0722e922127c185835cb55eb0c127d92ae1b
serozhenka <sergeykaoff@gmail.com>
2023-11-18 12:18:49 +0200

    feat: generator initial setup

---
+++ backend/generator/app/src/main/java/com/gradle/Generator/Main.java
@@ -0,0 +1,12 @@
+package com.gradle.Generator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+    }
+}
