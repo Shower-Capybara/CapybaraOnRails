e5a8d39b6434c181a894c7b280197ad53db7a3f7
Kolia913 <mynew1mail470@gmail.com>
2023-11-24 22:53:54 +0200

    changed tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
