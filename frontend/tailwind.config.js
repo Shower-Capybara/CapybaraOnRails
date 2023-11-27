e38527e4ef6edc9c4adf0d0a69e10746490825c6
Kolia913 <mynew1mail470@gmail.com>
2023-11-28 00:09:59 +0200

    feat: started adding tailwind theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#122083'
+      }
+    }
   },
   variants: {
     extend: {}
