c8883358a9a107a47af554dc644cbd85f3ca9a27
Tim <44300490+TimofiyJ@users.noreply.github.com>
2023-11-28 01:28:57 +0200

    feat: add colors, add icons

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#122083'
+        primary: '#122083',
+        font_black: '#000000',
+        font_white: '#FFFFF',
+        dropdown_grey: '#AEAEAE',
+        background_grey: '#B3B3B3',
+        free_space_green: '#15B42E',
+        yellow_design: '#FFAF44',
+        yellow_status: '#F9BE26',
+        green_status: '#15B42E',
+        red_status: '#F41919'
       }
     }
   },
