ac500a3031db83153fe1393ed4ff7e66da904f10
Дмитро <dmytro.ivasikiv.pz.2021@lpnu.ua>
2023-12-02 13:58:47 +0200

    feat: Kolya help me please

--- frontend/src/components/EventList/EventListActions.vue
+++ frontend/src/components/EventList/EventListActions.vue
@@ -1,6 +1,15 @@
 <script lang="ts" setup>
 import AppButton from '@/components/atoms/AppButton.vue'
 import SettingsIcons from '@/components/icons/SettingsIcon.vue'
+
+import { ref } from 'vue'
+import Settings from '@/components/Settings/Settings.vue'
+
+const showSettings = ref(false)
+
+const openSettings = () => {
+  showSettings.value = true
+}
 </script>
 <template>
   <div class="flex flex-row justify-between items-center w-full">
@@ -10,8 +19,9 @@ import SettingsIcons from '@/components/icons/SettingsIcon.vue'
     <div class="h-8 w-32">
       <AppButton text="Додати вхід" />
     </div>
-    <div class="cursor-pointer">
+    <div class="cursor-pointer" @click="openSettings">
       <SettingsIcons />
     </div>
+    <Settings v-if="showSettings" />
   </div>
 </template>
---
+++ frontend/src/components/Settings/Settings.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col w-full h-full">
+    <div class="flex justify-between mb-4">
+      <h1 class="text-xl font-bold">Налаштування</h1>
+      <a href="#" class="text-blue-500">Історія обслуговування</a>
+    </div>
+    <div class="flex flex-col gap-4">
+      <div class="mb-4">
+        <label class="text-sm font-medium">Кількість кас</label>
+        <input type="number" class="w-full form-control" />
+      </div>
+      <div class="mb-4">
+        <label class="text-sm font-medium">Кількість входів</label>
+        <input type="number" class="w-full form-control" />
+      </div>
+      <div class="mb-4">
+        <label class="text-sm font-medium">Час обслуговування</label>
+        <input type="number" class="w-full form-control" />
+      </div>
+      <div class="mb-4">
+        <label class="text-sm font-medium">Стратегія появи</label>
+        <select class="w-full form-control">
+          <option value="стратегія 1">Стратегія 1</option>
+          <option value="стратегія 2">Стратегія 2</option>
+        </select>
+      </div>
+    </div>
+    <div class="flex justify-end">
+      <button class="btn btn-primary">Зберегти і перезапустити</button>
+      <button class="btn btn-secondary">Скасувати зміни</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// -----
+</script>
