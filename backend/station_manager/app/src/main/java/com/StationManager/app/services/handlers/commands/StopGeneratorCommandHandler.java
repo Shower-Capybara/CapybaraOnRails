a36d3604668b2843f44d70641e017ca7fc96c20e
Erikh <adestalion@gmail.com>
2023-12-07 23:47:12 +0200

    fix (station-manager): added NotImplementedError() inside handles

--- backend/station_manager/app/src/main/java/com/StationManager/app/services/handlers/commands/StopGeneratorCommandHandler.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/services/handlers/commands/StopGeneratorCommandHandler.java
@@ -1,4 +1,12 @@
 package com.StationManager.app.services.handlers.commands;
 
-public class StopGeneratorCommandHandler {
+import com.StationManager.app.domain.commands.StopGeneratorCommand;
+import com.StationManager.app.services.unitofwork.UnitOfWork;
+import kotlin.NotImplementedError;
+
+public class StopGeneratorCommandHandler implements CommandHandler<StopGeneratorCommand> {
+    @Override
+    public void handle(StopGeneratorCommand command, UnitOfWork uow) {
+        throw new NotImplementedError();
+    }
 }
