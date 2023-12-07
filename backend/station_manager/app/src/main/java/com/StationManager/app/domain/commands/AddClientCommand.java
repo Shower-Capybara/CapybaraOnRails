7e83f4c0e459655d9d9c517b8a71fda45a2502d9
Erikh <adestalion@gmail.com>
2023-12-07 20:23:38 +0200

    fix (station-manager): add hallId to AddClientCommand

--- backend/station_manager/app/src/main/java/com/StationManager/app/domain/commands/AddClientCommand.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/domain/commands/AddClientCommand.java
@@ -4,8 +4,10 @@ import com.StationManager.app.domain.client.Client;
 
 public class AddClientCommand extends Command {
     public Client client;
+    public Integer hallId;
 
-    public AddClientCommand(Client client) {
+    public AddClientCommand(Client client, Integer hallId) {
         this.client = client;
+        this.hallId = hallId;
     }
 }
