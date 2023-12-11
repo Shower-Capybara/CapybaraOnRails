9654a2d768ddb9f890436b54895e2a2aef6df584
Erikh <adestalion@gmail.com>
2023-12-11 16:28:43 +0200

    feat (station-manager): add ticket-office open/close commands

---
+++ backend/station_manager/shared/src/main/java/com/StationManager/shared/domain/commands/OpenTicketOfficeCommand.java
@@ -0,0 +1,18 @@
+package com.StationManager.shared.domain.commands;
+
+import com.StationManager.shared.domain.train_station.TicketOffice;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenTicketOfficeCommand extends Command {
+    public Integer ticketOfficeId;
+
+    @JsonCreator
+    public OpenTicketOfficeCommand(
+        @JsonProperty(value = "ticketOffice", required = true) Integer ticketOfficeId
+    ) {
+        this.ticketOfficeId = ticketOfficeId;
+    }
+}
