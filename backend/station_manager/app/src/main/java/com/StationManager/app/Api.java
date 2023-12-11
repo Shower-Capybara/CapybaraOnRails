ae66ef89986f60b7c0f9f0df3db7213201d8fcb9
Erikh <adestalion@gmail.com>
2023-12-11 03:22:09 +0200

    feat (station-manager): add end-points

--- backend/station_manager/app/src/main/java/com/StationManager/app/Api.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/Api.java
@@ -1,20 +1,24 @@
 package com.StationManager.app;
 
+import com.StationManager.app.controllers.HallController;
+import com.StationManager.app.controllers.SocketController;
+import com.StationManager.app.controllers.TicketOfficeController;
 import com.StationManager.app.services.MessageBus;
+import com.StationManager.app.services.command_listener.ForwardPubSub;
 import io.javalin.Javalin;
 import io.javalin.websocket.WsContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPubSub;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Api {
-    private static final Logger logger = LoggerFactory.getLogger(Api.class);
-    static Set<WsContext> wsSessions = new HashSet<>();
+    public static final Logger logger = LoggerFactory.getLogger(Api.class);
+    public static Set<WsContext> wsSessions = new HashSet<>();
 
     public static void main(String[] args) {
         MessageBus.bootstrap();
@@ -22,36 +26,28 @@ public class Api {
         Javalin app = Javalin.create().start(Settings.JAVALIN_PORT);
         var redisListener = new Jedis(Settings.REDIS_HOST, Settings.REDIS_PORT);
 
-        app.get("/", ctx -> {
-            logger.info("Request finished");
+        app.routes(() -> {
+            path("/train_station", () -> {
+                path("/hall/{h_id}", () -> {
+                    get("/get-state", HallController::getState);
+                    put("/save-state", HallController::saveState);
+                    put("/configure-system", HallController::configureSystem);
+                    get("/get-service-history", HallController::getServiceHistory);
+                    post("/create-entry", HallController::createEntry);
+                    post("/create-ticket-office", TicketOfficeController::createTicketOffice);
+                    path("/open-ticket-office/{t_id}", () ->
+                        put(TicketOfficeController::openTicketOffice));
+                    path("/close-ticket-office/{t_id}", () ->
+                        put(TicketOfficeController::closeTicketOffice));
+                });
+            });
+            ws("/events", SocketController.configEvents());
         });
-        app.ws("/events", ws -> {
-            ws.onConnect(ctx -> {
-                wsSessions.add(ctx);
-                ctx.enableAutomaticPings(10000, TimeUnit.MILLISECONDS);
-                logger.info("Established: " + ctx.getSessionId());
-            });
-            ws.onMessage(ctx -> {
-                logger.info("Message received: " + ctx.getSessionId());
-            });
-            ws.onClose(ctx -> {
-                wsSessions.remove(ctx);
-                logger.info("Closed: " + ctx.getSessionId());
-            });
-            ws.onError(ctx -> logger.info("Error " + ctx.getSessionId()));
-        });
-        redisListener.psubscribe(new JedisPubSub() {
-            @Override
-            public void onPMessage(String pattern, String channel, String message) {
-                broadcastMessage(message);
-                logger.info(message);
-            }
-        }, Settings.getEventChannel("*"));
-    }
 
-    private static void broadcastMessage(String message) {
-        wsSessions.forEach(session -> {
-            session.send(message);
-        });
+        redisListener.psubscribe(new ForwardPubSub(), Settings.getEventChannel("*"));
     }
 }
+
+
+
+
