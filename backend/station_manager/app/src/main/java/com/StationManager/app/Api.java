11dc5df9fe884be12aa541416b43a997b3dacdd7
Erikh <adestalion@gmail.com>
2023-12-11 10:53:35 +0200

    fix (station-manager): fix ticket_offices end-points

--- backend/station_manager/app/src/main/java/com/StationManager/app/Api.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/Api.java
@@ -6,20 +6,11 @@ import com.StationManager.app.controllers.TicketOfficeController;
 import com.StationManager.app.services.MessageBus;
 import com.StationManager.app.services.command_listener.ForwardPubSub;
 import io.javalin.Javalin;
-import io.javalin.websocket.WsContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Api {
-    public static final Logger logger = LoggerFactory.getLogger(Api.class);
-    public static Set<WsContext> wsSessions = new HashSet<>();
-
     public static void main(String[] args) {
         MessageBus.bootstrap();
 
@@ -28,23 +19,25 @@ public class Api {
 
         app.routes(() -> {
             path("/train_station", () -> {
-                path("/hall/{h_id}", () -> {
-                    get("/get-state", HallController::getState);
-                    put("/save-state", HallController::saveState);
-                    put("/configure-system", HallController::configureSystem);
-                    get("/get-service-history", HallController::getServiceHistory);
-                    post("/create-entry", HallController::createEntry);
-                    post("/create-ticket-office", TicketOfficeController::createTicketOffice);
-                    path("/open-ticket-office/{t_id}", () ->
-                        put(TicketOfficeController::openTicketOffice));
-                    path("/close-ticket-office/{t_id}", () ->
-                        put(TicketOfficeController::closeTicketOffice));
+                path("/halls/{h_id}", () -> {
+                    get("/get_state", HallController::getState);
+                    put("/save_state", HallController::saveState);
+                    put("/configure_system", HallController::configureSystem);
+                    get("/get_service_history", HallController::getServiceHistory);
+                    post("/create_entry", HallController::createEntry);
+                    path("/ticket_offices", ()->{
+                        post(TicketOfficeController::createTicketOffice);
+                        path("/{t_id}/open", () ->
+                            put(TicketOfficeController::openTicketOffice));
+                        path("/{t_id}/close", () ->
+                            put(TicketOfficeController::closeTicketOffice));
+                    });
                 });
             });
             ws("/events", SocketController.configEvents());
         });
 
-        redisListener.psubscribe(new ForwardPubSub(), Settings.getEventChannel("*"));
+        redisListener.psubscribe(new ForwardPubSub(SocketController.wsSessions), Settings.getEventChannel("*"));
     }
 }
 
