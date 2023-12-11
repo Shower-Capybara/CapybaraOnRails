339f82c128604f937126446a53964ae882d74098
Erikh <adestalion@gmail.com>
2023-12-11 10:54:47 +0200

    fix (station-manager): move logger and wsSessions to SocketController

--- backend/station_manager/app/src/main/java/com/StationManager/app/controllers/SocketController.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/controllers/SocketController.java
@@ -1,15 +1,21 @@
 package com.StationManager.app.controllers;
 
+import com.StationManager.app.Api;
 import io.javalin.websocket.WsConfig;
+import io.javalin.websocket.WsContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
-import static com.StationManager.app.Api.logger;
-import static com.StationManager.app.Api.wsSessions;
-
 public class SocketController {
-    public static  Consumer<WsConfig> configEvents() {
+    public static final Logger logger = LoggerFactory.getLogger(Api.class);
+    public static Set<WsContext> wsSessions = new HashSet<>();
+
+    public static Consumer<WsConfig> configEvents() {
         return ws -> {
             ws.onConnect(ctx -> {
                 wsSessions.add(ctx);
