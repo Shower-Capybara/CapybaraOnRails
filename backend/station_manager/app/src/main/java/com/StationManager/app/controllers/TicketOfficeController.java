047586da20c6a37a471f4fd444006b980df1505e
Erikh <adestalion@gmail.com>
2023-12-11 16:30:36 +0200

    feat (station-manager): add ticket-office controllers and simulator logic + some import(idk) to gradle

--- backend/station_manager/app/src/main/java/com/StationManager/app/controllers/TicketOfficeController.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/controllers/TicketOfficeController.java
@@ -1,20 +1,36 @@
 package com.StationManager.app.controllers;
 
+import com.StationManager.app.Settings;
+import com.StationManager.app.services.command_listener.Json;
+import com.StationManager.app.services.command_listener.RedisPubSub;
+import com.StationManager.shared.domain.commands.CloseTicketOfficeCommand;
+import com.StationManager.shared.domain.commands.OpenTicketOfficeCommand;
+import com.StationManager.shared.domain.commands.StopGeneratorCommand;
+import com.StationManager.shared.domain.events.ClientServedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.http.Context;
+import redis.clients.jedis.Jedis;
 
 public class TicketOfficeController {
+    static Jedis redisPublisher = new Jedis(Settings.REDIS_HOST, Settings.REDIS_PORT);
+    static ObjectMapper objectMapper = Json.getObjectMapper();
+
+
     public static void createTicketOffice(Context ctx) {
         // Implementation
         ctx.status(200).result("Ticket office created successfully");
     }
 
-    public static void closeTicketOffice(Context ctx) {
-        // Implementation
-        ctx.status(200).result("Ticket office closed successfully");
+    public static void closeTicketOffice(Context ctx) throws JsonProcessingException {
+        CloseTicketOfficeCommand closeTicketOfficeCommand = new CloseTicketOfficeCommand(Integer.parseInt(ctx.body()));
+        redisPublisher.publish(Settings.getCommandChannel(CloseTicketOfficeCommand.class.getSimpleName()), objectMapper.writeValueAsString(closeTicketOfficeCommand));
+        ctx.status(200);
     }
 
-    public static void openTicketOffice(Context ctx) {
-        // Implementation
-        ctx.status(200).result("Ticket office opened successfully");
+    public static void openTicketOffice(Context ctx) throws JsonProcessingException {
+        OpenTicketOfficeCommand openTicketOfficeCommand = new OpenTicketOfficeCommand(Integer.parseInt(ctx.body()));
+        redisPublisher.publish(Settings.getCommandChannel(OpenTicketOfficeCommand.class.getSimpleName()), objectMapper.writeValueAsString(openTicketOfficeCommand));
+        ctx.status(200);
     }
 }
