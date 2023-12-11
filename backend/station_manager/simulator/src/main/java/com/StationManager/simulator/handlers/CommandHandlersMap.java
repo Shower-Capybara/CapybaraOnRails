e9699780c25fddade596bb5fdef1c15078862904
Erikh <adestalion@gmail.com>
2023-12-11 16:28:58 +0200

    feat (station-manager): add CommandHandlersMap

---
+++ backend/station_manager/simulator/src/main/java/com/StationManager/simulator/handlers/CommandHandlersMap.java
@@ -0,0 +1,22 @@
+package com.StationManager.simulator.handlers;
+
+import com.StationManager.app.services.handlers.commands.CommandHandler;
+import com.StationManager.shared.domain.commands.Command;
+import com.StationManager.shared.domain.events.Event;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class CommandHandlersMap {
+    private final Map<Class<? extends Command>, Object> map = new HashMap<>();
+    public <T extends Command> void put(Class<T> key, Consumer<T> value) {
+        map.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Command> Consumer<T> get(T command) {
+        return (Consumer<T>) map.getOrDefault(command.getClass(), null);
+    }
+}
