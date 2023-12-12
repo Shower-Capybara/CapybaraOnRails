fc59ade8138dd0b085b323c0c33e43773dfc2021
ErikhPetrushynets <132948467+ErikhPetrushynets@users.noreply.github.com>
2023-12-12 11:07:54 +0200

    feat (station-manager): add random client and stop generator (#56)

--- backend/station_manager/simulator/src/main/java/com/StationManager/simulator/core/hall/HallSimulator.java
+++ backend/station_manager/simulator/src/main/java/com/StationManager/simulator/core/hall/HallSimulator.java
@@ -1,18 +1,26 @@
 package com.StationManager.simulator.core.hall;
 
 import com.StationManager.shared.domain.client.Client;
+import com.StationManager.shared.domain.client.Privilegy;
 import com.StationManager.shared.domain.commands.AddClientCommand;
+import com.StationManager.shared.services.unitofwork.PostgresUnitOfWork;
 import com.StationManager.simulator.Json;
 import com.StationManager.simulator.Settings;
 import com.StationManager.simulator.core.hall.policies.Policy;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.StationManager.simulator.core.hall.policies.RandomPolicy;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+import java.util.UUID;
+
 public class HallSimulator implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(HallSimulator.class);
+    private PostgresUnitOfWork uow = new PostgresUnitOfWork();
+    private final Integer maxCapacity = 5;
 
     private final Integer id;
     private Policy schedulingPolicy;
@@ -23,32 +31,47 @@ public class HallSimulator implements Runnable {
         this.id = id;
         this.schedulingPolicy = schedulingPolicy;
         this.redis = redis;
+        this.schedulingPolicy = new RandomPolicy(3.0, 5.0);
     }
 
     private Client generateClient() {
-        return new Client(
-            null,
-            "First name", // change to random
-            "Last name", // change to random
-            null, // change to random
-            null
-        );
+        List<Privilegy> privileges = uow.getPrivilegyRepository().getAll();
+
+        if (Math.random() < 0.5) {
+            return new Client(
+                null,
+                UUID.randomUUID().toString().substring(0, 8),
+                UUID.randomUUID().toString().substring(0, 8),
+                null,
+                null
+            );
+        } else {
+            Privilegy randomPrivilege = privileges.get((int) (Math.random() * (privileges.size())));
+            return new Client(
+                null,
+                UUID.randomUUID().toString().substring(0, 8),
+                UUID.randomUUID().toString().substring(0, 8),
+                randomPrivilege,
+                null
+            );
+        }
     }
 
     @Override
     public void run() {
         while (true) {
             // TODO: check if the hall is not exceeding the capacity norm
-            var client = this.generateClient();
-            var command = new AddClientCommand(client, this.id);
-
-            try {
-                var channel = Settings.getCommandChannel(command.getClass().getSimpleName());
-                this.redis.publish(channel, this.objectMapper.writeValueAsString(command));
-                logger.info(String.format("Sent AddClientCommand to %s channel", channel));
-                Thread.sleep(Math.round(this.schedulingPolicy.getSeconds() * 1000));
-            } catch (InterruptedException | JsonProcessingException e) {
-                break;
+            if(uow.getClientRepository().getAll().size() < maxCapacity){
+                var client = this.generateClient();
+                var command = new AddClientCommand(client, this.id);
+                try {
+                    var channel = Settings.getCommandChannel(command.getClass().getSimpleName());
+                    this.redis.publish(channel, this.objectMapper.writeValueAsString(command));
+                    logger.info(String.format("Sent AddClientCommand to %s channel", channel));
+                    Thread.sleep(Math.round(this.schedulingPolicy.getSeconds() * 1000));
+                } catch (InterruptedException | JsonProcessingException e) {
+                    break;
+                }
             }
         }
     }
