405b3e5e01ace79da44ca25155b8d57b63dc6e94
Tim <44300490+TimofiyJ@users.noreply.github.com>
2023-12-12 02:26:00 +0200

    fix: update websocket

--- backend/station_manager/app/src/main/java/com/StationManager/app/Settings.java
+++ backend/station_manager/app/src/main/java/com/StationManager/app/Settings.java
@@ -1,11 +1,11 @@
 package com.StationManager.app;
 
 public class Settings {
-    public static int JAVALIN_PORT = Integer.parseInt(System.getenv("JAVALIN_PORT"));
-    public static String REDIS_HOST = System.getenv("REDIS_HOST");
-    public static Integer REDIS_PORT = Integer.parseInt(System.getenv("REDIS_PORT"));
-    public static String REDIS_EVENTS_CHANNEL_PREFIX = System.getenv("REDIS_EVENTS_CHANNEL_PREFIX");
-    public static String REDIS_COMMANDS_CHANNEL_PREFIX = System.getenv("REDIS_COMMANDS_CHANNEL_PREFIX");
+    public static int JAVALIN_PORT = Integer.parseInt(System.getenv("JAVALIN_PORT").trim());
+    public static String REDIS_HOST = System.getenv("REDIS_HOST").trim();
+    public static Integer REDIS_PORT = Integer.parseInt(System.getenv("REDIS_PORT").trim());
+    public static String REDIS_EVENTS_CHANNEL_PREFIX = System.getenv("REDIS_EVENTS_CHANNEL_PREFIX").trim();
+    public static String REDIS_COMMANDS_CHANNEL_PREFIX = System.getenv("REDIS_COMMANDS_CHANNEL_PREFIX").trim();
 
     public static String getEventChannel(String eventName) {
         return String.format("%s:%s", Settings.REDIS_EVENTS_CHANNEL_PREFIX, eventName);
